8bf761dc2bb294c156fa318aeff9e232242c4239
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-06-16 22:02:37 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import  styles from "./App.module.css";
+import Exercise from "./components/Exercise";
+
+function App() {
+    return (
+        <>
+            <header className={styles.header}>
+                <h1>Workout</h1>
+            </header>
+            <div className={styles.exercisesContainer}>
+                <Exercise name="DB Bench Press" expand={false} />
+                <Exercise name="Back Squat" expand={false}/>
+                <Exercise name="Military Press" expand={false} />
+                <Exercise name="Military Press" expand={false} />
+                <Exercise name="Military Press" expand={false} />
+                <Exercise name="Military Press" expand={false} />
+                <Exercise name="Military Press" expand={false} />
+                <Exercise name="Military Press" expand={false} />
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Exercise.jsx
@@ -0,0 +1,49 @@
+import styles from "./Exercise.module.css";
+import { useState } from "react";
+function Exercise({ name,expand }) {
+    const [isActive, setActive] = useState(expand);
+    function handleClick(){
+        setActive(!isActive);
+        expand = isActive;
+    }
+
+
+    return (
+
+        <>
+            <div className={styles.exerciseContainer}>
+                <h3>{name}</h3>
+                <icon onClick={handleClick} className={`${styles.exerciseExpandIcon} `}>{isActive===true ? "-" : "+"}</icon>
+            </div>
+
+            <div>
+                <div className={`${isActive===true ? styles.exerciseGrid :styles.dropdownFalse}`}>
+                    <div className={styles.weight}>Weight (Kg)</div>
+                    <div className={styles.weightInput}>
+                        <input type="number" step="0.25" ></input>
+                    </div>
+                    <div className={styles.reps}>
+                        Reps
+                    </div>
+                    <div className={styles.repsInput}>
+                            <input type="number" step="0.25" ></input>
+                        </div>
+                    <div className={styles.sets}>
+                        Sets
+                    </div>
+                    <div className={styles.setsInput}>
+                            <input type="number" step="0.25" ></input>
+                        </div>
+                    <div className={styles.submitExercise}>
+                        <button>Add Exercise</button>
+                    </div>
+                    <div className={styles.clearExercise}>
+                        <button>Clear</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Exercise;
