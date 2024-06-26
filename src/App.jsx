572f446a9ec1fafb87b5dbf06665f28787f547a5
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-06-26 13:15:17 +0100

    initialise config files and set up server

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@ import styles from "./App.module.css";
 import Exercise from "./components/Exercise";
 
 function App() {
-    return (
-        <>
-            <header className={styles.header}>
-                <h1>Workout</h1>
-            </header>
-            <div className={styles.exercisesContainer}>
-                <Exercise name="DB Bench Press" expand={false} />
-                <Exercise name="Back Squat" expand={false} />
-                <Exercise name="Military Press" expand={false} />
-                <Exercise name="Military Press" expand={false} />
-                <Exercise name="Military Press" expand={false} />
-                <Exercise name="Military Press" expand={false} />
-                <Exercise name="Military Press" expand={false} />
-                <Exercise name="Military Press" expand={false} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Workout</h1>
+      </header>
+      <div className={styles.exercisesContainer}>
+        <Exercise name="DB Bench Press" expand={false} />
+        <Exercise name="Back Squat" expand={false} />
+        <Exercise name="Military Press" expand={false} />
+        <Exercise name="Romanian Deadlift" expand={false} />
+        <Exercise name="Preacher Curl" expand={false} />
+        <Exercise name="Lat Pulldown" expand={false} />
+        <Exercise name="Zottman Curls" expand={false} />
+        <Exercise name="Lat DB Raises" expand={false} />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -1,88 +1,84 @@
+/* eslint-disable react/prop-types */
 import styles from "./Exercise.module.css";
 import { useState } from "react";
 function Exercise({ name, expand }) {
-    const [weightValue, setWeightValue] = useState(null);
-    const [repValue, setRepValue] = useState(null);
-    const [setsValue, setSetsValue] = useState(null);
-    const [isActive, setActive] = useState(expand);
-    function handleClick() {
-        setActive(!isActive);
-        expand = isActive;
-    }
+  const [weightValue, setWeightValue] = useState(null);
+  const [repValue, setRepValue] = useState(null);
+  const [setsValue, setSetsValue] = useState(null);
+  const [isActive, setActive] = useState(expand);
+  function handleClick() {
+    setActive(!isActive);
+    expand = isActive;
+  }
 
-    function handleChangeWeight(e) {
-        setWeightValue(e.target.value);
-    }
-    function handleChangeReps(e) {
-        setRepValue(e.target.value);
-    }
-    function handleChangeSets(e) {
-        setSetsValue(e.target.value);
-    }
+  function handleChangeWeight(e) {
+    setWeightValue(e.target.value);
+  }
+  function handleChangeReps(e) {
+    setRepValue(e.target.value);
+  }
+  function handleChangeSets(e) {
+    setSetsValue(e.target.value);
+  }
 
-    function handleClear() {
-        setWeightValue("");
-        setRepValue("");
-        setSetsValue("");
-    }
+  function handleClear() {
+    setWeightValue("");
+    setRepValue("");
+    setSetsValue("");
+  }
 
-    return (
-        <>
-            <div className={styles.exerciseContainer}>
-                <h3>{name}</h3>
-                <icon
-                    onClick={handleClick}
-                    className={`${styles.exerciseExpandIcon} `}
-                >
-                    {isActive === true ? "-" : "+"}
-                </icon>
-            </div>
+  return (
+    <>
+      <div className={styles.exerciseContainer}>
+        <h3>{name}</h3>
+        <icon onClick={handleClick} className={`${styles.exerciseExpandIcon} `}>
+          {isActive === true ? "-" : "+"}
+        </icon>
+      </div>
 
-            <div>
-                <div
-                    className={`${
-                        isActive === true
-                            ? styles.exerciseGrid
-                            : styles.dropdownFalse
-                    }`}
-                >
-                    <div className={styles.weight}>Weight (Kg)</div>
-                    <div className={styles.weightInput}>
-                        <input
-                            type="number"
-                            step="0.25"
-                            value={weightValue}
-                            onChange={handleChangeWeight}
-                        ></input>
-                    </div>
-                    <div className={styles.reps}>Reps</div>
-                    <div className={styles.repsInput}>
-                        <input
-                            type="number"
-                            step="0.25"
-                            value={repValue}
-                            onChange={handleChangeReps}
-                        ></input>
-                    </div>
-                    <div className={styles.sets}>Sets</div>
-                    <div className={styles.setsInput}>
-                        <input
-                            type="number"
-                            step="0.25"
-                            value={setsValue}
-                            onChange={handleChangeSets}
-                        ></input>
-                    </div>
-                    <div className={styles.submitExercise}>
-                        <button>Add Exercise</button>
-                    </div>
-                    <div className={styles.clearExercise}>
-                        <button onClick={handleClear}>Clear</button>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+      <div>
+        <div
+          className={`${
+            isActive === true ? styles.exerciseGrid : styles.dropdownFalse
+          }`}
+        >
+          <div className={styles.weight}>Weight (Kg)</div>
+          <div className={styles.weightInput}>
+            <input
+              type="number"
+              step="0.25"
+              value={weightValue}
+              onChange={handleChangeWeight}
+            ></input>
+          </div>
+          <div className={styles.reps}>Reps</div>
+          <div className={styles.repsInput}>
+            <input
+              type="number"
+              step="0.25"
+              value={repValue}
+              onChange={handleChangeReps}
+            ></input>
+          </div>
+          <div className={styles.sets}>Sets</div>
+          <div className={styles.setsInput}>
+            <input
+              type="number"
+              step="0.25"
+              value={setsValue}
+              onChange={handleChangeSets}
+            ></input>
+          </div>
+          <div className={styles.submitExercise}>
+            <button>Add Exercise</button>
+          </div>
+          <div className={styles.clearExercise}>
+            <button onClick={handleClear}>Clear</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Exercise;
